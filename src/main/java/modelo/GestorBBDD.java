be8cb508bd3f316620b761e455358d2dd5484eed
laurimoreni <laura.morenogo@elorrieta-errekamari.com>
2019-04-10 13:27:48 +0200

    creada consulta para buscar un cliente por dni

--- src/main/java/modelo/GestorBBDD.java
+++ src/main/java/modelo/GestorBBDD.java
@@ -2,6 +2,7 @@ package modelo;
 
 import java.util.ArrayList;
 import Reto4Grupo1BBDD.AlojamientoBBDD;
+import Reto4Grupo1BBDD.ClienteBBDD;
 import Reto4Grupo1BBDD.ModificarBBDD;
 
 public class GestorBBDD {
@@ -27,6 +28,15 @@ public class GestorBBDD {
 	
 	public ArrayList<String> cargarListaDestinos() {
 		return modificarBBDD.cargarListaDestinos();
+	}
+	
+	public Cliente cargarCliente(int dni) {
+		ClienteBBDD clienteBBDD = modificarBBDD.cargarCliente(dni);
+		Cliente cliente = new Cliente(
+			clienteBBDD.getDni(),
+			clienteBBDD.getPassword()
+		);
+		return cliente;
 	}
 	
 	/*public int insertarDatos(String string) {
