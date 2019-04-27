3344a78a576278b2abe6fe96cf0283e7687c380b
laurimoreni <laura.morenogo@elorrieta-errekamari.com>
2019-04-27 19:38:56 +0200

    renombrada vista SeleccionAlojamiento2 a SeleccionAlojamiento y mostrar habitaciones disponibles del alojamiento seleccionado

--- src/main/java/aplicacion/ControladorSelHabitacion.java
+++ src/main/java/aplicacion/ControladorSelHabitacion.java
@@ -9,8 +9,11 @@ import org.controlsfx.control.textfield.TextFields;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXRippler;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.effects.JFXDepthManager;
 
 import core.Principal;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -23,6 +26,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import modelo.Alojamiento;
 import modelo.Habitacion;
@@ -44,12 +48,12 @@ public class ControladorSelHabitacion implements Initializable {
 
     @FXML
     void atras(MouseEvent event) {
-
+    	Principal.aplicacion.CambiarScene("SeleccionAlojamiento.fxml");
     }
 
     @FXML
     void siguiente(MouseEvent event) {
-
+    	Principal.aplicacion.CambiarScene("InfoReserva.fxml");
     }
     
     @Override
@@ -67,7 +71,7 @@ public class ControladorSelHabitacion implements Initializable {
     	ColumnConstraints column1 = new ColumnConstraints();
     	column1.setPercentWidth(100);
     	grid.getColumnConstraints().add(column1);
-    	grid.setGridLinesVisible(true);
+    	grid.setStyle("-fx-background-color: transparent;");
     	
     	// hace que los anchorpanes rellenen todo el espacio disponible en el padre
         AnchorPane.setTopAnchor(grid, 0.0);
@@ -75,13 +79,64 @@ public class ControladorSelHabitacion implements Initializable {
         AnchorPane.setLeftAnchor(grid, 0.0);
         AnchorPane.setRightAnchor(grid, 0.0);
         
-		// cargamos la arraylist de alojamientos
-		ArrayList<Habitacion> habitaciones = Principal.modelo.gestorBBDD.cargarListaHabitaciones(alojamiento.getCodAlojamiento());
+		// cargamos la arraylist de habitaciones
+		ArrayList<Habitacion> habitaciones = Principal.modelo.gestorBBDD.habitacionesDisponibles(alojamiento.getCodAlojamiento());
         
     	for(int i=0; i<habitaciones.size(); i++) {
     		
-    		// FALTA
-    		// copiar de controladorSelAlojamiento
+    		Habitacion habitacion = habitaciones.get(i);
+    		AnchorPane anchorPane = new AnchorPane();
+    		
+    		// añadimos la accion que se ejecutara al clickar el panel
+    		//anchorPane = añadirListenerSeleccion(anchorPane, habitacion);
+    		
+    		// label - nombre de la habitacion
+    		Text nombreHab = new Text(habitacion.getNombre());
+    		nombreHab.setLayoutX(170);
+    		nombreHab.setLayoutY(30);
+    		nombreHab.setFill(Paint.valueOf("#07c"));
+    		nombreHab.setStyle("-fx-font: 25 arial;");
+    		nombreHab.setLayoutX(170);
+    		nombreHab.setLayoutY(35);
+    		
+    		FontAwesomeIconView iconoCama = new FontAwesomeIconView(FontAwesomeIcon.BED);
+    		iconoCama.setLayoutX(180);
+    		iconoCama.setLayoutY(60);
+    		iconoCama.setSize("20");
+    		iconoCama.setFill(Paint.valueOf("#555555"));		
+    		
+    		Text camas = new Text("Codigo " + habitacion.getCodHabitacion() + ", Cantidad " + habitacion.getCantidad() + ", " + habitacion.getCtaCamasSimples() + " Camas Simples");
+    		camas.setLayoutX(200);
+    		camas.setLayoutY(60);
+    		
+    		// precio
+    		Text precio = new Text("100€");
+    		precio.setLayoutX(675);
+    		precio.setLayoutY(80);
+    		precio.setStyle("-fx-font: 20 arial;");
+    		precio.setFill(Paint.valueOf("#0ab21b"));
+    		
+    		// label - descripcion del habitacion
+    		Text descripcion = new Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. ");
+    		descripcion.setLayoutX(170);
+    		descripcion.setLayoutY(85);
+    		
+    		// añade los componentes al anchorpane
+        	anchorPane.getChildren().addAll(nombreHab, descripcion, camas, iconoCama, precio);
+        	
+        	AnchorPane paneSuperior = new AnchorPane();
+        	paneSuperior.getChildren().addAll(anchorPane);
+        	
+        	JFXRippler rippler = new JFXRippler(anchorPane);
+        	rippler.setRipplerFill(Paint.valueOf("#AAAAAA"));
+        	paneSuperior.getChildren().add(rippler);
+        	
+    		anchorPane.setStyle("-fx-background-color: #fff; -fx-padding: 5px; -fx-border-insets: 5px; -fx-background-insets: 5px; -fx-border-radius:  1 1 1 1; -fx-background-radius: 5 5 5 5;");
+    		JFXDepthManager.setDepth(anchorPane, 1);
+    		anchorPane.setPrefWidth(contenedor.getWidth()-20);
+    		anchorPane.setMaxWidth(contenedor.getWidth()-20);
+    		// añade el anchorpane al grid
+    		grid.add(paneSuperior, 0, i);
     		
     	}
 	}
