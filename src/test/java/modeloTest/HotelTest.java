47e559a5bd04f5ab0fd4a61731eb6d0b80a18aff
IN1DM3B_01 <IN1DM3B_01@H015112.NET>
2019-04-30 09:02:39 +0200

    Arreglados teses de los objetos

--- src/test/java/modeloTest/HotelTest.java
+++ src/test/java/modeloTest/HotelTest.java
@@ -8,29 +8,11 @@ import modelo.Hotel;
 
 public class HotelTest {
 
-	Hotel hotel = new Hotel(1, "Bilbao", "Hotel Bilbao", "cremas", 20, 20, 1, 1.5f, 2, 3, 4, 5, 2);
+	Hotel hotel = new Hotel(1, "Bilbao", "Hotel Bilbao", "cremas", 20, 20, 1, 1.5f, 2, 3, 4, 5, 2, "www.kakota");
 	
 	@Test
 	public void testConstructor() {
 		assertEquals(2, hotel.getEstrellas());
-	}
-
-	@Test
-	public void testCodAlojamiento() {
-		hotel.setCodAlojamiento(1);
-		assertEquals(1, hotel.getCodAlojamiento());
-	}
-	
-	@Test
-	public void testNombre() {
-		hotel.setNombre("Hotel Bilbao");
-		assertEquals("Hotel Bilbao", hotel.getNombre());
-	}
-	
-	@Test
-	public void testUbicacion() {
-		hotel.setUbicacion("Bilbao");
-		assertEquals("Bilbao", hotel.getUbicacion());
 	}
 	
 	@Test
