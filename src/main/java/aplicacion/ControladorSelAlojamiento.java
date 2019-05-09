8cc956fc526275560225314c9d50119608107e79
laurimoreni <laura.morenogo@elorrieta-errekamari.com>
2019-05-09 20:03:53 +0200

    creado elemento 'Card' en la vista y cambiado controladorSelAlojamiento para que lo use para cargar los alojamientos

--- src/main/java/aplicacion/ControladorSelAlojamiento.java
+++ src/main/java/aplicacion/ControladorSelAlojamiento.java
@@ -18,11 +18,7 @@ import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.effects.JFXDepthManager;
 
 import core.Principal;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DateCell;
@@ -39,14 +35,10 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.web.WebView;
 import javafx.util.Callback;
 import modelo.Alojamiento;
-import modelo.Apartamento;
-import modelo.Casa;
-import modelo.Estancia;
 import modelo.Habitacion;
-import modelo.Hotel;
+import vista.Card;
 
 public class ControladorSelAlojamiento implements Initializable {
 	
@@ -54,22 +46,16 @@ public class ControladorSelAlojamiento implements Initializable {
     private AnchorPane contenedor;
 
     @FXML
-    private Pane paneBusqueda;
+    private Pane paneBusqueda, paneFiltros;
 
     @FXML
     private JFXTextField textCiudad;
     
     @FXML
-    private JFXDatePicker fechaEntrada;
-    
-    @FXML
-    private JFXDatePicker fechaSalida;
+    private JFXDatePicker fechaEntrada, fechaSalida;
 
     @FXML
     private JFXButton btnBuscar;
-    
-    @FXML
-    private Pane paneFiltros;
     
     @FXML
     private Label lblSaludo;
@@ -92,19 +78,17 @@ public class ControladorSelAlojamiento implements Initializable {
 		fechaEntrada = deshabilitarFechas(fechaEntrada, LocalDate.now());
 		fechaSalida = deshabilitarFechas(fechaSalida, LocalDate.now().plusDays(1));
 		
-		cargarAutocompletar();
 		Text busqueda = new Text("No ha realizado ninguna busqueda aun.");
 		busqueda.setLayoutX(245);
 		busqueda.setLayoutY(70);
 		busqueda.setFont(new Font("arial",16));
 		contenedor.getChildren().setAll(busqueda);
-//		cargarAlojamientos();
+		
+		cargarAutocompletar();
 	}
     
     @FXML
     void seleccion(ActionEvent event) {
-   	 	// cambiar los dias deshabilitados en el datepicker 'fechaSalida'
-		// cuando se selecciona una fecha en el datepicker 'fechaEntrada'
     	fechaSalida = deshabilitarFechas(fechaSalida, fechaEntrada.getValue().plusDays(1));
 		fechaSalida.setValue(fechaEntrada.getValue().plusDays(1));
     }
@@ -124,12 +108,9 @@ public class ControladorSelAlojamiento implements Initializable {
     @FXML
     void IniciarCerrar(ActionEvent event) {
     	Principal.aplicacion.controladorSelAlojamiento=this;
-    	if(Principal.modelo.cliente==null)
-		{
+    	if(Principal.modelo.cliente == null) {
 			Principal.aplicacion.CargarSceneLogin();
-		}
-		else
-		{
+		} else {
 			Principal.iniciarPrograma();
 		}
     }
@@ -181,206 +162,29 @@ public class ControladorSelAlojamiento implements Initializable {
      */
 	public void cargarAlojamientos() {
     
-    	GridPane grid = crearGrid();
-    	contenedor.getChildren().setAll(grid);
-       
 		ArrayList<Alojamiento> alojamientos = Principal.modelo.gestorBBDD.cargarAlojamientos(textCiudad.getText());
+    	GridPane grid = crearGrid();
         
-    	for(int i=0; i<alojamientos.size(); i++) {
+    	for(int i = 0; i<alojamientos.size(); i++) {
     		
     		Alojamiento alojamiento = alojamientos.get(i);
+    		ArrayList<Habitacion> habitaciones = buscarHabDisponibles(alojamiento);
+    		alojamiento.setHabitaciones(habitaciones);
     		
-    		AnchorPane anchorPane = new AnchorPane();
-    		anchorPane = añadirListenerSeleccion(anchorPane, alojamiento);
-    		anchorPane.setPrefHeight(170);
+    		Card card = new Card(alojamiento, habitaciones, fechaEntrada, fechaSalida);
     		
-    		// label - nombre del alojamiento
-    		Text nombreHotel = crearNombre(alojamiento.getNombre());
-    		
-    		//Icono de ubicacion
-    		WebView imagen = crearImagen(alojamiento.getImgurl());
-    		
-    		// Ubicacion del alojamiento
-    		FontAwesomeIconView iconoUbicacion = crearIconoUbicacion();
-    		
-    		Text ubicacion = new Text(alojamiento.getUbicacion());
-    		ubicacion.setLayoutX(245);
-    		ubicacion.setLayoutY(70);
-    		
-    		Hyperlink mapa = new Hyperlink("Ver Mapa");
-    		int tamanoUbicacion = (int) ((int) ubicacion.getBoundsInLocal().getMaxX()+240+10);
-    		mapa.setLayoutX(tamanoUbicacion);
-    		mapa.setLayoutY(54);
-    		mapa.setOnAction((e) -> {
-    			verMapa(alojamiento);
-            });    		
-    		
-    		//Estrellas del hotel (Si es un hotel claro)
-    		int tamanoNombre = (int) ((int) nombreHotel.getBoundsInLocal().getMaxX()+220+5);
-    		
-	    	if(alojamiento instanceof Hotel) {
-	    		
-	    		// muestra los iconos estrella
-		    	int coordX=tamanoNombre+10;
-	    		for(int e=0;e<((Hotel) alojamiento).getEstrellas();e++) {
-		    		FontAwesomeIconView iconoEstrella = new FontAwesomeIconView(FontAwesomeIcon.STAR);
-		    		iconoEstrella.setLayoutX(coordX);
-			    	iconoEstrella.setLayoutY(40);
-			    	iconoEstrella.setSize("15");
-			    	iconoEstrella.setFill(Paint.valueOf("#feba02"));
-			    	anchorPane.getChildren().add(iconoEstrella);
-			    	coordX=coordX+15;
-		    	}
-	    		
-	    		// Muestra las habitaciones disponibles
-	    		Label lblHabitaciones = new Label("Habitaciones disponibles:");
-	    		lblHabitaciones.setStyle("-fx-font-weight: bold");
-	    		lblHabitaciones.setLayoutX(225);
-	    		lblHabitaciones.setLayoutY(130);
-	    		anchorPane.getChildren().add(lblHabitaciones);
-	    		
-	    		ArrayList<Habitacion> habitaciones = buscarHabDisponibles(alojamiento);
-	    		alojamiento.setHabitaciones(habitaciones);
-        		String str = mostrarHabitaciones(habitaciones);
-        		Text habDisponibles = new Text(str);
-        		habDisponibles.setLayoutX(225);
-        		habDisponibles.setLayoutY(165);
-        		habDisponibles.maxWidth(50);
-        		anchorPane.getChildren().add(habDisponibles);
-	    		
-	    	} else if(alojamiento instanceof Apartamento) {
-	    		
-	    		// Muestra las habitaciones disponibles
-	    		Label lblHabitaciones = new Label("Habitaciones:");
-	    		lblHabitaciones.setStyle("-fx-font-weight: bold");
-	    		lblHabitaciones.setLayoutX(325);
-	    		lblHabitaciones.setLayoutY(130);
-	    		anchorPane.getChildren().add(lblHabitaciones);
-	    		
-	    		ArrayList<Habitacion> habitaciones = buscarHabDisponibles(alojamiento);
-	    		alojamiento.setHabitaciones(habitaciones);
-        		String str1 = mostrarHabitaciones(habitaciones);
-        		Text habDisponibles = new Text(str1);
-        		habDisponibles.setLayoutX(325);
-        		habDisponibles.setLayoutY(165);
-        		habDisponibles.maxWidth(50);
-        		anchorPane.getChildren().add(habDisponibles);
-	    			
-    			// Muestra las estancias
-	    		Label lblEstancias = new Label("Estancias:");
-	    		lblEstancias.setStyle("-fx-font-weight: bold");
-	    		lblEstancias.setLayoutX(225);
-	    		lblEstancias.setLayoutY(130);
-	    		anchorPane.getChildren().add(lblEstancias);
-	    		
-	    		String str2 = mostrarEstancias(((Apartamento)alojamiento).getEstancias());
-        		Text estancias = new Text(str2);
-        		estancias.setLayoutX(225);
-        		estancias.setLayoutY(165);
-        		estancias.maxWidth(50);
-        		anchorPane.getChildren().add(estancias);
-    			
-    			// muestra el icono llave
-		    	int coordX = tamanoNombre + 10;
-	    		FontAwesomeIconView iconoLlave = new FontAwesomeIconView(FontAwesomeIcon.KEY);
-	    		iconoLlave.setLayoutX(coordX);
-	    		iconoLlave.setLayoutY(40);
-	    		iconoLlave.setSize("15");
-	    		iconoLlave.setFill(Paint.valueOf("#555555"));
-		    	anchorPane.getChildren().add(iconoLlave);
-			    	
-	    	} else if(alojamiento instanceof Casa) {
-	    		
-	    		// Muestra las habitaciones disponibles
-	    		Label lblHabitaciones = new Label("Habitaciones:");
-	    		lblHabitaciones.setStyle("-fx-font-weight: bold");
-	    		lblHabitaciones.setLayoutX(325);
-	    		lblHabitaciones.setLayoutY(130);
-	    		anchorPane.getChildren().add(lblHabitaciones);
-	    		
-	    		ArrayList<Habitacion> habitaciones = buscarHabDisponibles(alojamiento);
-	    		alojamiento.setHabitaciones(habitaciones);
-        		String str1 = mostrarHabitaciones(habitaciones);
-        		Text habDisponibles = new Text(str1);
-        		habDisponibles.setLayoutX(325);
-        		habDisponibles.setLayoutY(165);
-        		habDisponibles.maxWidth(50);
-        		anchorPane.getChildren().add(habDisponibles);
-	    		
-    			// Muestra las estancias
-        		Label lblEstancias = new Label("Estancias:");
-        		lblEstancias.setStyle("-fx-font-weight: bold");
-	    		lblEstancias.setLayoutX(225);
-	    		lblEstancias.setLayoutY(130);
-	    		anchorPane.getChildren().add(lblEstancias);
-	    		
-	    		String str2 = mostrarEstancias(((Casa)alojamiento).getEstancias());
-	    		Text estancias = new Text(str2);
-        		estancias.setLayoutX(225);
-        		estancias.setLayoutY(165);
-        		estancias.maxWidth(50);
-        		anchorPane.getChildren().add(estancias);
-	    		
-	    		// muestra el icono casa
-	    		int coordX=tamanoNombre+10;
-	    		FontAwesomeIconView iconoCasa = new FontAwesomeIconView(FontAwesomeIcon.HOME);
-	    		iconoCasa.setLayoutX(coordX);
-	    		iconoCasa.setLayoutY(40);
-	    		iconoCasa.setSize("15");
-	    		iconoCasa.setFill(Paint.valueOf("#555555"));
-		    	anchorPane.getChildren().add(iconoCasa);
-	    	}
-    		
-    		// precio
-	    	
-	    	
-    		Text precio = new Text(alojamiento.getPrecioHabBarata() + "€");
-    		precio.setLayoutX(660);
-    		precio.setLayoutY(40);
-    		precio.setStyle("-fx-font: 20 arial;");
-    		precio.setFill(Paint.valueOf("#0ab21b"));
-    		
-    		// label - descripcion del alojamiento
-    		Label descripcion = new Label(alojamiento.getDescripcion());
-    		descripcion.setLayoutX(225);
-    		descripcion.setLayoutY(75);
-    		descripcion.setPrefWidth(500);
-    		descripcion.setPrefHeight(50);
-    		descripcion.setWrapText(true);
-    		
-    		JFXButton btnVerMas = new JFXButton("Ver Más");
-    		btnVerMas.setLayoutX(650);
-    		btnVerMas.setLayoutY(anchorPane.getPrefHeight());
-    		btnVerMas.setPrefSize(80, 40);
-    		btnVerMas.setStyle("-fx-background-color: #f57c00; -fx-text-fill: white;");
-    		btnVerMas.setOnAction((e) -> {
-    			verInfo(alojamiento);
-            });
-    		
-    		// añade los componentes al anchorpane
-        	anchorPane.getChildren().addAll(nombreHotel, descripcion, ubicacion, iconoUbicacion, precio, imagen, mapa, btnVerMas);
-        	
         	AnchorPane paneSuperior = new AnchorPane();
-        	paneSuperior.getChildren().addAll(anchorPane);
+        	JFXRippler rippler = new JFXRippler(card);
         	
-        	JFXRippler rippler = new JFXRippler(anchorPane);
-        	rippler.setRipplerFill(Paint.valueOf("#AAAAAA"));
+    		JFXDepthManager.setDepth(card, 1);
+    		paneSuperior.getChildren().addAll(card);
         	paneSuperior.getChildren().add(rippler);
-        	
-    		anchorPane.setStyle("-fx-background-color: #fff; -fx-padding: 5px; -fx-border-insets: 5px; -fx-background-insets: 5px; -fx-border-radius:  1 1 1 1; -fx-background-radius: 5 5 5 5;");
-    		JFXDepthManager.setDepth(anchorPane, 1);
-    		anchorPane.setPrefWidth(750);
-    		anchorPane.setMaxWidth(750);
+        	rippler.setRipplerFill(Paint.valueOf("#AAAAAA"));  
+    		
+			grid.add(card, 0, i); 	
 
-    		// añade el anchorpane al grid
-    		grid.add(paneSuperior, 0, i);
     	}
 	}
-	
-	 void verMapa(Alojamiento alojamiento) {
-		Principal.modelo.reserva.setAlojamiento(alojamiento);
-		Principal.aplicacion.verMapa("Mapa.fxml");
-    }
 	
 	/**
      * Crea y añade el grid al anchorpane 'contenedor', creado por defecto
@@ -388,57 +192,12 @@ public class ControladorSelAlojamiento implements Initializable {
      */
     public GridPane crearGrid() {
     	GridPane grid = new GridPane();
-    	
-    	// hace que la columna 1 del grid ocupe todo el espacio disponible en el padre
     	ColumnConstraints column1 = new ColumnConstraints();
     	column1.setPercentWidth(100);
     	grid.getColumnConstraints().add(column1);
-    	
-    	// pone el color de fondo transparente
     	grid.setStyle("-fx-background-color: transparent;");
-    	
+    	contenedor.getChildren().setAll(grid);
     	return grid;
-    }
-    
-    /**
-     * 
-     * @param nombre
-     * @return
-     */
-    public Text crearNombre(String nombre) {
-    	Text text = new Text(nombre);
-		text.setFill(Paint.valueOf("#07c"));
-		text.setFont(new Font("arial",18));
-		text.setLayoutX(225);
-		text.setLayoutY(40);
-		return text;
-    }
-    
-    /**
-     * 
-     * @param url
-     * @return
-     */
-    public WebView crearImagen(String url) {
-    	WebView imagen = new WebView();
-		imagen.setLayoutX(10);
-		imagen.setLayoutY(10);
-		imagen.setPrefSize(200, 200);
-		imagen.getEngine().loadContent("<img src=\"" + url + "\" width=\"190\" height=\"180\" frameborder=\"0\" style=\"border:0\"></img>", "text/html");
-		return imagen;
-    }
-    
-    /**
-     * 
-     * @return
-     */
-    public FontAwesomeIconView crearIconoUbicacion() {
-    	FontAwesomeIconView iconoUbicacion = new FontAwesomeIconView(FontAwesomeIcon.MAP_MARKER);
-		iconoUbicacion.setLayoutX(225);
-		iconoUbicacion.setLayoutY(70);
-		iconoUbicacion.setSize("20");
-		iconoUbicacion.setFill(Paint.valueOf("#555555"));
-		return iconoUbicacion;
     }
     
     /**
@@ -455,57 +214,7 @@ public class ControladorSelAlojamiento implements Initializable {
     
     /**
      * 
-     * @param habitaciones
-     * @return
      */
-    public String mostrarHabitaciones(ArrayList<Habitacion> habitaciones) {
-    	String str = "";
-		for (Habitacion s : habitaciones) {
-			str += s.getNombre().toLowerCase() + " x " + s.getCantidad() + "\n";
-		}
-		return str;
-    }
-    
-    /**
-     * 
-     * @param habitaciones
-     * @return
-     */
-    public String mostrarEstancias(ArrayList<Estancia> estancias) {
-    	String str = "";
-		for (Estancia s : estancias) {
-			str += s.getNombre().toLowerCase() + " x "+ s.getCantidad() + "\n";
-		}
-		return str;
-    }
-	
-	/**
-	 * 
-	 * @param anchorPane
-	 * @param alojamiento
-	 * @param fechaEntrada
-	 * @param fechaSalida
-	 * @return
-	 */
-	public AnchorPane añadirListenerSeleccion(AnchorPane anchorPane, Alojamiento alojamiento) {
-		anchorPane.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<Event>(){
-			@Override
-			public void handle(Event event) {
-				verInfo(alojamiento);
-			}
-		});
-		return anchorPane;
-	}
-	
-	public void verInfo(Alojamiento alojamiento) {
-		Date fechaEntradaDate = Date.valueOf(fechaEntrada.getValue());
-		Date fechaSalidaDate = Date.valueOf(fechaSalida.getValue());
-		Principal.modelo.reserva.setAlojamiento(alojamiento);
-		Principal.modelo.reserva.setFechaEntrada(fechaEntradaDate);
-		Principal.modelo.reserva.setFechaSalida(fechaSalidaDate);
-		Principal.aplicacion.CambiarScene("PaneInfo.fxml");
-	}
-
 	public void SesionIniciada() {
 		lblSaludo.setText("Hola, " + Principal.modelo.cliente.getDni());
 		lblSesion.setText("Cerrar Sesion");
---
+++ src/main/java/vista/Card.java
@@ -0,0 +1,269 @@
+package vista;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+
+import core.Principal;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
+import modelo.Alojamiento;
+import modelo.Apartamento;
+import modelo.Casa;
+import modelo.Estancia;
+import modelo.Habitacion;
+import modelo.Hotel;
+
+public class Card extends AnchorPane implements Initializable {
+	
+	@FXML
+    private AnchorPane card;
+
+    @FXML
+    private ImageView imagen;
+
+    @FXML
+    private Text nombre, ubicacion, precio, habitaciones, estancias;
+
+    @FXML
+    private Hyperlink mapa;
+
+    @FXML
+    private Label descripcion;
+    
+    @FXML
+    private JFXButton btnVer;
+    
+    private Alojamiento alojamiento;
+    
+    private ArrayList<Habitacion> ArrayHabitaciones;
+    
+    private JFXDatePicker fechaEntrada, fechaSalida;
+
+    @FXML
+    void verAlojamiento(ActionEvent event) {
+    	verInfo(this.alojamiento);
+    }
+    
+	public Card(Alojamiento alojamiento, ArrayList<Habitacion> habitaciones, JFXDatePicker fechaEntrada, JFXDatePicker fechaSalida) {
+		
+		this.alojamiento = alojamiento;
+		this.ArrayHabitaciones = habitaciones;
+		this.fechaEntrada = fechaEntrada;
+		this.fechaSalida = fechaSalida;
+		
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vista/Alojamiento.fxml"));
+	    fxmlLoader.setRoot(this);
+	    fxmlLoader.setController(this);
+
+	    try {
+	        fxmlLoader.load();
+	    } catch (IOException e) {
+	        throw new RuntimeException(e);
+	    }
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		setNombre(this.alojamiento.getNombre());
+		setImagen(this.alojamiento.getImgurl());
+		setUbicacion(this.alojamiento.getUbicacion());
+		setMapa(this.alojamiento);
+		setDescripcion(this.alojamiento.getDescripcion());
+		setPrecio(this.alojamiento.getPrecioHabBarata() + "€");
+		setIconoAloj(this.alojamiento);
+		setHabitaciones(this.alojamiento, this.ArrayHabitaciones);
+		setEstancias(this.alojamiento);
+		setListener();
+	}
+	
+	/**
+	 * 
+	 * @param card
+	 * @param alojamiento
+	 * @param fechaEntrada
+	 * @param fechaSalida
+	 * @return
+	 */
+	public AnchorPane añadirListenerSeleccion(AnchorPane card, Alojamiento alojamiento) {
+		card.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<Event>(){
+			@Override
+			public void handle(Event event) {
+				verInfo(alojamiento);
+			}
+		});
+		return card;
+	}
+	
+	public void setListener() {
+		this.card = añadirListenerSeleccion(this.card, this.alojamiento);
+	}
+
+	public void setImagen(String url) {
+		this.imagen.setImage(new Image(url));
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre.setText(nombre);
+	}
+	
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion.setText(ubicacion); 
+	}
+	
+	public void setMapa(Alojamiento alojamiento) {
+		int tamanoUbicacion = (int) ((int) this.ubicacion.getBoundsInLocal().getMaxX()+this.ubicacion.getLayoutX() + 5);
+		mapa.setLayoutX(tamanoUbicacion);
+		mapa.setOnAction((e) -> {
+			Principal.modelo.reserva.setAlojamiento(alojamiento);
+			Principal.aplicacion.verMapa("Mapa.fxml");
+        }); 
+	}
+	
+	public void setDescripcion(String desc) {
+		this.descripcion.setText(desc);
+	}
+	
+	public void setPrecio(String precio) {
+		this.precio.setText(precio);
+	}
+	
+	public void setIconoAloj(Alojamiento alojamiento) {
+		int coordX = (int) ((int) this.nombre.getBoundsInLocal().getMaxX()+this.nombre.getLayoutX()+10);
+		if(alojamiento instanceof Hotel) {
+    		for(int i = 0; i < ((Hotel) alojamiento).getEstrellas(); i++) {
+    			FontAwesomeIconView iconoEstrella = crearIconoEstrella(coordX);
+		    	card.getChildren().add(iconoEstrella);
+		    	coordX = coordX + 15;
+	    	}
+		} else if(alojamiento instanceof Apartamento) {
+    		FontAwesomeIconView iconoLlave = crearIconoLlave(coordX);
+	    	card.getChildren().add(iconoLlave);
+		} else if(alojamiento instanceof Casa) {
+    		FontAwesomeIconView iconoCasa = crearIconoCasa(coordX);
+	    	card.getChildren().add(iconoCasa);
+		}
+	}
+	
+	public void setHabitaciones(Alojamiento alojamiento, ArrayList<Habitacion> habitaciones) {
+		String str = mostrarHabitaciones(habitaciones);
+		this.habitaciones.setText(str);
+	}
+	
+	public void setEstancias(Alojamiento alojamiento) {
+		if(alojamiento instanceof Hotel) {
+			this.card.getChildren().remove(this.estancias);
+		} else if(alojamiento instanceof Apartamento) {
+			String str = mostrarEstancias(((Apartamento)alojamiento).getEstancias());
+			this.estancias.setText(str);
+		} else if(alojamiento instanceof Casa) {	
+			String str = mostrarEstancias(((Casa)alojamiento).getEstancias());
+			this.estancias.setText(str);
+		}
+	}
+	
+	public void setButton() {
+		this.btnVer.setLayoutY(card.getBoundsInLocal().getHeight()-70);
+	}
+	
+	/**
+	 * 
+	 * @param coordX
+	 * @return
+	 */
+	public FontAwesomeIconView crearIconoEstrella(int coordX) {
+		FontAwesomeIconView iconoEstrella = new FontAwesomeIconView(FontAwesomeIcon.STAR);
+		iconoEstrella.setLayoutX(coordX);
+    	iconoEstrella.setLayoutY(40);
+    	iconoEstrella.setSize("15");
+    	iconoEstrella.setFill(Paint.valueOf("#feba02"));
+    	return iconoEstrella;
+	}
+	
+	/**
+	 * 
+	 * @param coordX
+	 * @return
+	 */
+	public FontAwesomeIconView crearIconoLlave(int coordX) {
+		FontAwesomeIconView iconoLlave = new FontAwesomeIconView(FontAwesomeIcon.KEY);
+		iconoLlave.setLayoutX(coordX);
+    	iconoLlave.setLayoutY(40);
+    	iconoLlave.setSize("15");
+    	iconoLlave.setFill(Paint.valueOf("#555555"));
+    	return iconoLlave;
+	}
+	
+	/**
+	 * 
+	 * @param coordX
+	 * @return
+	 */
+	public FontAwesomeIconView crearIconoCasa(int coordX) {
+		FontAwesomeIconView iconoLlave = new FontAwesomeIconView(FontAwesomeIcon.HOME);
+		iconoLlave.setLayoutX(coordX);
+    	iconoLlave.setLayoutY(40);
+    	iconoLlave.setSize("15");
+    	iconoLlave.setFill(Paint.valueOf("#555555"));
+    	return iconoLlave;
+	}
+	
+    /**
+     * 
+     * @param habitaciones
+     * @return
+     */
+    public String mostrarHabitaciones(ArrayList<Habitacion> habitaciones) {
+    	String str = "";
+		for (Habitacion s : habitaciones) {
+			str += s.getNombre().toLowerCase() + " x " + s.getCantidad() + "\n";
+		}
+		return str;
+    }
+    
+    /**
+     * 
+     * @param estancias
+     * @return
+     */
+    public String mostrarEstancias(ArrayList<Estancia> estancias) {
+    	String str = "";
+		for (Estancia s : estancias) {
+			str += s.getNombre().toLowerCase() + " x "+ s.getCantidad() + "\n";
+		}
+		return str;
+    }
+    
+    /**
+     * 
+     * @param alojamiento
+     */
+	public void verInfo(Alojamiento alojamiento) {
+		Date fechaEntradaDate = Date.valueOf(this.fechaEntrada.getValue());
+		Date fechaSalidaDate = Date.valueOf(this.fechaSalida.getValue());
+		Principal.modelo.reserva.setAlojamiento(alojamiento);
+		Principal.modelo.reserva.setFechaEntrada(fechaEntradaDate);
+		Principal.modelo.reserva.setFechaSalida(fechaSalidaDate);
+		Principal.aplicacion.CambiarScene("PaneInfo.fxml");
+	}
+	
+}
